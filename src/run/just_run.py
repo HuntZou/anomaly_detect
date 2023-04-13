e61c5468dc1b8922c13de45a930a5542927c4807
HuntZou <zouheng613@163.com>
2023-04-13 20:10:11 +0800

    enable cudnn for speed up

--- src/run/just_run.py
+++ src/run/just_run.py
@@ -10,8 +10,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '.
 
 from modules.ad_module import STLNet_AD
 from config import TrainConfigures
+from datasets.mvtec import ADMvTec
 
-# TrainConfigures.device = torch.device('cpu')
+TrainConfigures.device = torch.device('cuda:0')
 
 net = STLNet_AD(in_channels=3, pretrained=True, output_stride=16).to(TrainConfigures.device)
 
@@ -27,20 +28,48 @@ def test_mock():
 
 
 def train_mock():
-    for i in range(1):
-        optimizer = torch.optim.Adam(params=net.parameters())
-        input = torch.randn([5, 3, 256, 256]).to(TrainConfigures.device)
-        gtmaps = torch.zeros([5, 256, 256]).to(TrainConfigures.device)
-        labels = torch.ones([5]).to(TrainConfigures.device)
+    optimizer = torch.optim.Adam(params=net.parameters())
+    batch_size = 5
+    for i in range(15):
+        inputs = torch.randn([batch_size, 3, 256, 256]).to(TrainConfigures.device)
+        gtmaps = torch.zeros([batch_size, 256, 256]).to(TrainConfigures.device)
+        labels = torch.ones([batch_size]).to(TrainConfigures.device)
         start = time.time()
-        anorm_heatmap, score_map = net(input)
+        anorm_heatmap, score_map = net(inputs)
         optimizer.zero_grad()
         loss = train.fcdd_loss(anorm_heatmap, score_map, gtmaps, labels)
         loss.backward()
         optimizer.step()
-        print(f'train done, cost: {time.time() - start}')
+        print(f'{time.ctime()} epoch {i}, train cost: {time.time() - start}')
+
+
+def train_real():
+    class_name_idx = 1
+    batch_size = 2
+    class_name = TrainConfigures.dataset.classes[class_name_idx]
+    print(f'class_name: {class_name}')
+    dataset = ADMvTec(
+        root=os.path.abspath(TrainConfigures.dataset.dataset_path), normal_class=class_name_idx, preproc='lcnaug1',
+        supervise_mode='malformed_normal_gt', noise_mode='confetti', online_supervision=True,
+        nominal_label=TrainConfigures.nominal_label, only_test=False
+    )
+    train_loader = torch.utils.data.DataLoader(dataset=dataset.train_set, batch_size=batch_size, shuffle=True,
+                                               num_workers=0, pin_memory=True, drop_last=False,
+                                               prefetch_factor=2, persistent_workers=TrainConfigures.dataset.worker_num > 0)
+
+    optimizer = torch.optim.Adam(params=net.parameters())
+    for n_batch, data in enumerate(train_loader):
+        inputs, labels, gtmaps = data[0].to(TrainConfigures.device, non_blocking=True), data[1], data[2].to(TrainConfigures.device, non_blocking=True)
+        start = time.time()
+        anorm_heatmap, score_map = net(inputs)
+        optimizer.zero_grad()
+        loss = train.fcdd_loss(anorm_heatmap, score_map, gtmaps, labels)
+        loss.backward()
+        optimizer.step()
+        print(f'{time.ctime()} iter {n_batch}, train cost: {time.time() - start}')
 
 
 if __name__ == '__main__':
     # test_mock()
     train_mock()
+    # train_real()
--- src/utils.py
+++ src/utils.py
@@ -292,7 +292,7 @@ def manual_random_seed():
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.enabled = True
 
 
 class DatasetConfig(ABC):
